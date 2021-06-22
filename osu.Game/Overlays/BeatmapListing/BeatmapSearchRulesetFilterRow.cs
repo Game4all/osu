66672bbce04059bb1669cfc3c6a0de0fa79df108
Lucas A <game4allyt@gmail.com>
2021-06-22 19:57:03 +0200

    Localize beatmap listing ruleset search filter.

--- osu.Game/Overlays/BeatmapListing/BeatmapSearchRulesetFilterRow.cs
+++ osu.Game/Overlays/BeatmapListing/BeatmapSearchRulesetFilterRow.cs
@@ -3,6 +3,8 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Localisation;
 using osu.Game.Resources.Localisation.Web;
 using osu.Game.Rulesets;
 
@@ -10,6 +12,8 @@ namespace osu.Game.Overlays.BeatmapListing
 {
     public class BeatmapSearchRulesetFilterRow : BeatmapSearchFilterRow<RulesetInfo>
     {
+        private const string ruleset_filter_item_any = @"Any";
+
         public BeatmapSearchRulesetFilterRow()
             : base(BeatmapsStrings.ListingSearchFiltersMode)
         {
@@ -19,17 +23,29 @@ namespace osu.Game.Overlays.BeatmapListing
 
         private class RulesetFilter : BeatmapSearchFilter
         {
+            protected override TabItem<RulesetInfo> CreateTabItem(RulesetInfo value) => new RulesetFilterTabItem(value);
+
             [BackgroundDependencyLoader]
             private void load(RulesetStore rulesets)
             {
                 AddItem(new RulesetInfo
                 {
-                    Name = @"Any"
+                    Name = ruleset_filter_item_any
                 });
 
                 foreach (var r in rulesets.AvailableRulesets)
                     AddItem(r);
             }
+        }
+
+        private class RulesetFilterTabItem : FilterTabItem<RulesetInfo>
+        {
+            public RulesetFilterTabItem(RulesetInfo value)
+                : base(value)
+            {
+            }
+
+            protected override LocalisableString LabelFor(RulesetInfo ruleset) => ruleset.Name == ruleset_filter_item_any ? BeatmapsStrings.ModeAny : ruleset.Name;
         }
     }
 }
